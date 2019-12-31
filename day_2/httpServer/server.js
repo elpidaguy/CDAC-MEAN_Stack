34cf1c6cb317be5c3965142c73a776212f9ffee4
elpidaguy <exabytes.js@gmail.com>
2019-12-31 08:31:19 +0530

    Http server using node js

---
+++ day_2/httpServer/server.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+var onCreateServer = function (req, res) {
+    var path = url.parse(req.url).pathname;
+
+    fs.readFile(path.substr(1), (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' })
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data.toString());
+        }
+        res.end();
+    })
+
+}
+
+var server = http.createServer(onCreateServer);
+server.listen(8081);
+console.log("Http server is listening at: http://localhost:8081/");
